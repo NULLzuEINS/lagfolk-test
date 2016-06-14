7520744cb8b9f6b983a49a815cb62f5a2eece392
André Lademann <andre.lademann@netresearch.de>
2016-06-14 18:32:53 +0200

    Engine not as option

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,41 @@
 'use strict';
 
-var engine = grunt.option('engine') || 'slimerjs';
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish'),
+                jshintrc: '.jshintrc'
+            },
+            files: [
+                'Gruntfile.js',
+                'test/**/*.js',
+                'package.json'
+            ]
+        },
+        casper: {
+            options: {
+                test: true,
+                includes: 'test/inc/inc.js',
+                // post : 'test/inc/post.js',
+                // pre : 'test/inc/pre.js',
+                'log-level': 'warning',
+                'fail-fast': true,
+                concise: true,
+                engine: 'phantomjs'
+            },
 
-module.exports = function(grunt) {
-	grunt.initConfig({
-		jshint: {
-			options: {
-				reporter: require('jshint-stylish'),
-				jshintrc: '.jshintrc'
-			},
-			files: [
-				'Gruntfile.js',
-				'test/**/*.js',
-				'package.json'
-			]
-		},
-		casper : {
-			options : {
-				test : true,
-				includes : 'test/inc/inc.js',
-				// post : 'test/inc/post.js',
-				// pre : 'test/inc/pre.js',
-				'log-level' : 'warning',
-				'fail-fast' : true,
-				concise : true,
-				engine : engine
-			},
+            test: {
+                files: {
+                    'report/xunit/casper-results.xml': ['test/casper/*Test.js']
+                }
 
-			test : {
-				files : {
-					'report/xunit/casper-results.xml' : ['test/casper/*Test.js']
-				}
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-casper');
 
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-casper');
-
-	grunt.registerTask('test', ['jshint', 'casper']);
-	grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['jshint', 'casper']);
+    grunt.registerTask('default', ['test']);
 };
